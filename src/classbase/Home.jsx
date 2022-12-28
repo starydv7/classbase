24d78d4e71139ce31e624e6350f0cfe5927e09ca
starydv7 <pawanstar72926@gmail.com>
2022-12-28 16:50:24 +0530

    ClassBase COmponents

---
+++ src/classbase/Home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            userName: "Pawan",
+            
+        }
+     }
+  render() {
+    return (
+      <div>Home</div>
+    )
+  }
+}
+export default Home;
