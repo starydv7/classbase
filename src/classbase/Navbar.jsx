4e5f2842b27363067272787a33fc1eadb04fe6f3
starydv7 <pawanstar72926@gmail.com>
2022-12-29 11:22:05 +0530

    Pawan

--- src/classbase/Navbar.jsx
+++ src/classbase/Navbar.jsx
@@ -3,7 +3,7 @@ import styles from "../components/home.module.css";
 export class Navbar extends Component {
   render = () => (
     <div className={styles.container1}>
-      <h2> {this.props.name} todo list</h2>
+      <h2> {this.props.name}'s todo list</h2>
       {/* <button onClick={this.changeState}>Change</button> */}
     </div>
   );
