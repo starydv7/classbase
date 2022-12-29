c40fb2ffed2ba020c71841d947738f390d914975
starydv7 <pawanstar72926@gmail.com>
2022-12-29 12:28:02 +0530

    Pawan

--- src/classbase/Home.jsx
+++ src/classbase/Home.jsx
@@ -1,82 +1,73 @@
-import React, { Component } from 'react'
-import styles from "../components/home.module.css";
-import { Navbar } from './Navbar';
-class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          userName: "Pawan",
-          todoItems: [
-            { action: "Buy Phone", done: false },
-            { action: "Buy sim", done: true },
-            { action: "Dentist at 5 pm", done: false },
-            ],
-          newTodo:"",
-        };
-    }
-    updateValue = (e) => {
-        this.setState({ newTodo: e.target.value });
-        
-    }
-    changeState = () => {
-        this.setState({
-            userName:this.state.userName==="Pawan"?"Yadav":"PawanYadav",
-        })
+import React, { Component } from "react";
+import { Navbar } from "./Navbar";
+import { TodoRows } from "./TodoRows";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userName: "Pawan",
+      todoItems: [
+        { action: "Buy Milk", done: true },
+        { action: "Buy Sim Card", done: false },
+        { action: "Go to market", done: false },
+      ],
+      newTodo: "",
+    };
   }
-  toggleDone = (todo) => 
-    this.setState({
-      todoItems: this.state.todoItems.map((item) =>
-        item.action === todo.action ? { ...item, done: !item.done } : item),
-    });
-    todoRows= () =>
-        this.state.todoItems.map((item) => (
-            <tr key={item.action}>
-            <td>{item.action}</td>
-            <td>
-              <input type="checkbox"
-                checked={item.done}
-                onChange={()=>this.toggleDone(item) }
-              />
-            </td>
-            </tr>
-        ))
+
+  updateValue = (event) => {
+    this.setState({ newTodo: event.target.value });
+  };
+
   newTodo = () => {
     this.setState({
       todoItems: [
         ...this.state.todoItems,
-        {action:this.state.newTodo,done:false},
+        { action: this.state.newTodo, done: false },
       ],
-    })
-  }
-  render() {
-    <Navbar name={this.state.userName}/>
-      return (
-        <>
-         
-              <div className={styles.container2}>
-                  <input
-                      className={styles.container3}
-                      value={this.state.newTodo}
-                      onChange={this.updateValue}
+    });
+  };
 
-            />
-            <br />
-            <button onClick={this.newTodo}>Add ToDo</button>
-                  
-              </div>
-          <div>
-            <table className={styles.table}>
-              <thead>
-                <tr>
-                  <th>Task</th>
-                  <th>Complete</th>
-                </tr>
-              </thead>
-              <tbody>{this.todoRows()}</tbody>
-            </table>
-          </div>
-        </>
-      );
-  }
+  todoRows = () =>
+    this.state.todoItems.map((item) => (
+      <TodoRows key={item.action} item={item} callback={this.toggleDone} />
+    ));
+
+  toggleDone = (todo) =>
+    this.setState({
+      todoItems: this.state.todoItems.map((item) =>
+        item.action === todo.action ? { ...item, done: !item.done } : item
+      ),
+    });
+
+  render = () => (
+    <div className="container">
+      <div className="row">
+        <Navbar name={this.state.userName} />
+        <div className="col-12">
+          <input
+            className="form-control"
+            value={this.state.newToDo}
+            onChange={this.updateValue}
+          />
+          <button className="btn btn-primary" onClick={this.newTodo}>
+            Add
+          </button>
+        </div>
+        <div className="col-12">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Task</th>
+                <th>Complete</th>
+              </tr>
+            </thead>
+            <tbody>{this.todoRows()}</tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default Home;
--- src/classbase/Navbar.jsx
+++ src/classbase/Navbar.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import styles from "../components/home.module.css";
 export class Navbar extends Component {
   render = () => (
-    <div className={styles.container1}>
-      <h2> {this.props.name}'s todo list</h2>
-      {/* <button onClick={this.changeState}>Change</button> */}
+    <div className="col-12">
+      <h2 className="bg-primary text-white text-center p2">
+        {this.props.name}'s To do list
+      </h2>
     </div>
   );
 }
